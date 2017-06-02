44956d91f3d979087dbe5710f815ff214d237318
aamirakhtar <aamiradvantage@gmail.com>
2017-06-03 03:44:10 +0500

    Log Analyser Test commit

---
+++ LogAnalyserTest/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using log4net.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LogAnalyserTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static log4net.ILog log;
+
+        static HomeController()
+        {
+            log = log4net.LogManager.GetLogger("default_logger");
+            //log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);            
+            XmlConfigurator.Configure();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            try
+            {
+                string s = null;
+                s.ToString();
+
+                ViewBag.Message = "Your application description page.";
+            }
+            catch(Exception ex)
+            {
+                log.Error(ex);
+            }
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            log.Error("Custom error");
+            log.Info("Custom info");
+            log.Warn("custom warning");
+
+            return View();
+        }
+    }
+}
---
+++ LogAnalyserTest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LogAnalyserTest.MvcApplication" Language="C#" %>
